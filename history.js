c6d16228121f2a126e41b06c760cd50a18a5ba69
Josh Emerson <josh.j.emerson@gmail.com>
2013-03-25 12:08:25 +0000

    Ignore links with a fragment identifier

--- history.js
+++ history.js
@@ -1,11 +1,10 @@
 var addEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}}();
 
 // History Ajax Links
-historyAjax = [
-    'config' => [
-        'title' => 'History AJAX'
-    ]
-];
+historyAjax = [];
+historyAjax.config = {
+    site-title: 'History AJAX'
+};
 (function() {
     function findUpTag(el, tag) {
         while (el.parentNode) {
@@ -71,7 +70,7 @@ historyAjax = [
                 if (link.tagName !== 'A') {
                     link = findUpTag(link, "A");
                 }
-                if (link && link.href.indexOf(document.domain) !== -1 && link.target == '') {
+                if (link && link.href.indexOf(document.domain) !== -1 && link.target == '' && link.href.indexOf('#') == -1) {
                     history.pushState({ajax: true}, null, link.href);
                     loadPage(link.href);
                     e.preventDefault();            
--- page3/index.php
+++ page3/index.php
@@ -2,4 +2,5 @@
 <?php include_once('../includes/header.php') ?>
     <h1>Page Three</h1>
     <p>Yup, you guessed it, another page here.</p>
+    <p>Links to <a href="http://google.com">another domain</a>, with a <a href="/" target="_blank">target defined</a> or with a <a href="#main">fragment identifier</a> are ignored by history-ajax.</p>
 <?php include_once('../includes/footer.php') ?>
